11fc52f39cd4c40dee74dbb5573375d4be74380b
Krasser Tamás <krasser.tamas@gmail.com>
2015-11-08 23:39:27 +0100

    ADD release_date

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # FSND Project 1 - Create Movie Trailer site
-This project shows basic usage of python and classes in general
+This project shows basic usage of python and classes in general.
 ## How to run this project
-run `python entry.py` in your terminal *(Note: python is needed)
+run `python entry.py` in your terminal
+## Extra modifications
+I've added `release_date` to the class & template
--- movie.py
+++ movie.py
@@ -1,7 +1,8 @@
 class Movie():
     """Class that represents a movie"""
-    def __init__(self, title, story_line, poster_image_url, trailer_youtube_url):
+    def __init__(self, title, story_line, poster_image_url, trailer_youtube_url, release_date):
         self.title = title
         self.story_line = story_line
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
+        self.release_date = release_date
